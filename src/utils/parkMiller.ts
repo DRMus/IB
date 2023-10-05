1a882454d2adf7f2838057778be25f2a7274e159
DRMus <78694252+DRMus@users.noreply.github.com>
2023-10-05 21:42:39 +0400

    да

--- src/utils/parkMiller.ts
+++ src/utils/parkMiller.ts
@@ -3,7 +3,7 @@ export const parkMiller = (length = 10000): number[] => {
 
   for(let i = 0; i < length; i++) {
     let prevBit = i - 1 < 0 ? 1 : parkMillerSequence[i-1];
-    parkMillerSequence[i] = (16_807 * prevBit) * Math.abs(2_147_483_647 - 1);
+    parkMillerSequence[i] = (16_807 * prevBit) % 2_147_483_647 - 1;
   }
   return parkMillerSequence
 }
